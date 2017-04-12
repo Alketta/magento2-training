8cfa696ec25c64c9f53c5e88c9279129487e19de
DarkVisenya <chetta.aline@gmail.com>
2017-04-12 15:18:27 +0200

    Ajout Test soap

--- _extra/scripts/testModules/seller/soap.php
+++ _extra/scripts/testModules/seller/soap.php
@@ -30,6 +30,10 @@ $search = [
     ],
 ];
 
+$client->trainingSellerSellerRepositoryV1GetById(['objectId' => 1]);
+$client->trainingSellerSellerRepositoryV1GetByIdentifier(['objectIdentifier' => 'main']);
+$client->trainingSellerSellerRepositoryV1GetList($search);
+
 $client->trainingSellerSellerRepositoryV1Save($object);
 $client->trainingSellerSellerRepositoryV1GetByIdentifier(['objectIdentifier' => $object['object']['identifier']]);
 $client->trainingSellerSellerRepositoryV1DeleteByIdentifier(['objectIdentifier' => $object['object']['identifier']]);
