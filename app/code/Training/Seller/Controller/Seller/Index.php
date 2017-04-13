13a8099b045bb5410bf88bf8dc24c3d58116d40f
DarkVisenya <chetta.aline@gmail.com>
2017-04-13 12:01:49 +0200

    Ajout de critères de recherche pour List Seller

--- app/code/Training/Seller/Controller/Seller/Index.php
+++ app/code/Training/Seller/Controller/Seller/Index.php
@@ -2,6 +2,9 @@
 
 namespace Training\Seller\Controller\Seller;
 
+use Magento\Framework\Api\SortOrder;
+use Training\Seller\Api\Data\SellerInterface;
+
 /**
  * Action : seller/index
  *
@@ -37,6 +40,47 @@ class Index extends AbstractAction {
      */
     protected function getSearchCriteria()
     {
+        // get the asked filter name, with protection
+        $searchName = (string) $this->_request->getParam('search_name', '');
+        // retire tous les tags
+        $searchName = strip_tags($searchName);
+        // interdit les caractères permmettant les injections sql
+        $searchName = preg_replace('/[\'"%]/', '', $searchName);
+        // retire les espace avant le searchName
+        $searchName = trim($searchName);
+
+        // build the filter, if needed, and add it to the criteria
+        if ($searchName!== '') {
+            // build the filter for the name
+            $filters[] = $this->filterBuilder
+                ->setField(SellerInterface::FIELD_NAME)
+                ->setConditionType('like')
+                ->setValue("%$searchName%")
+                ->create();
+
+            // add the filter to the criteria
+            $this->searchCriteriaBuilder->addFilters($filters);
+        }
+
+        // get the asked sort order, with protection
+        $sortOrder = (string) $this->_request->getParam('sort_order');
+        $availableSort = [
+            SortOrder::SORT_ASC,
+            SortOrder::SORT_DESC,
+        ];
+        if (!in_array($sortOrder, $availableSort)) {
+            // mise en place par default de l'ordre croissant
+            $sortOrder = $availableSort[0];
+        }
+
+        // build the sort order and add it to the criteria
+        $sort = $this->sortOrderBuilder
+            ->setField(SellerInterface::FIELD_NAME)
+            ->setDirection($sortOrder)
+            ->create();
+        $this->searchCriteriaBuilder->addSortOrder($sort);
+
+        // build the criteria
         return $this->searchCriteriaBuilder->create();
     }
 
